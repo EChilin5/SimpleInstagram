a8a9e24f3017f0d99a0725a6564ae934a7668098
EChilin5 <edgar3ac@gmail.com>
2021-03-08 18:24:55 -0800

    able to query data

--- app/src/main/java/com/bignerdranch/android/simpleinstagram/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/simpleinstagram/MainActivity.java
@@ -3,12 +3,57 @@ package com.bignerdranch.android.simpleinstagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    private EditText etDescription;
+    private Button btnCaptureImage;
+    private Button btnSubmit;
+    private ImageView ivPosterImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etDescription = findViewById(R.id.etDescription);
+        btnCaptureImage = findViewById(R.id.btnCaptureImage);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        ivPosterImage = findViewById(R.id.ivPostImage);
+
+        queryPost();
+
+
+    }
+
+    private void queryPost() {
+
+        // Specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for(Post post: posts){
+                    Log.i(TAG, "Post: " + post.getDescription() + " USERNAME: " + post.getUser().getUsername());
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/bignerdranch/android/simpleinstagram/ParseApplication.java
+++ app/src/main/java/com/bignerdranch/android/simpleinstagram/ParseApplication.java
@@ -3,12 +3,16 @@ package com.bignerdranch.android.simpleinstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("3OlNmivNDa2BrUcgWl79QiNnLFJVVQfYnIK7BNRt")
